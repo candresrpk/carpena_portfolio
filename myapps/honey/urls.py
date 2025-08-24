d37fc4084c849bdf158df9a1bf28bcde7504cb1d
Carlos Reyes <andrespk3000@gmail.com>
2025-08-23 23:56:40 -0500

    transactions creation setup

--- myapps/honey/urls.py
+++ myapps/honey/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.users_dashboard, name='users_dashboard'),
     path('test/dashboard/', views.non_users_dashboard, name='non_users_dashboard'),
-    
+    path('create_transaction/', views.create_user_transaction, name='create_user_transaction'),
 ]
--- myapps/honey/views.py
+++ myapps/honey/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .forms import TransactionForm
 from .models import Transaction
 # Create your views here.
 
@@ -20,13 +21,14 @@ def users_dashboard(request):
     lend = transactions.filter(category=Transaction.CategoryChoices.LEND)
     
     
-    
+    create_user_transaction = TransactionForm()
     context = {
         'transactions': transactions,
         'income': income,
         'expenses': expenses,
         'borrow': borrow,
         'lend': lend,
+        'create_user_transaction': create_user_transaction,
     }
     
     return render(request, 'honey/users_dashboard.html', context)
@@ -34,3 +36,19 @@ def users_dashboard(request):
 
 def non_users_dashboard(request):
     return render(request, 'honey/non_users_dashboard.html')
+
+
+def create_user_transaction(request):
+    if not request.user.is_authenticated:
+        return redirect('users:login')
+    
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            return redirect('honey:users_dashboard')
+    else:
+        form = TransactionForm()
+    return redirect('honey:users_dashboard')
